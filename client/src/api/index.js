4d6f38c5c09148fc12873536ddf834cfa47a6a3b
abhinavkumar03 <ak2711474@gmail.com>
2023-06-18 08:00:09 +0530

    BaseUrl

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const BaseUrl = process.env.BaseUrl || 'http://localhost:5000';
+const API = axios.create({ baseURL: BaseUrl  })
 
 API.interceptors.request.use((req) => {
     try {
